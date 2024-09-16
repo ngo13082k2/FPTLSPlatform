e61c5704783ac86c3b404c648f87b729e038b560
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-09-16 21:52:54 +0700

    Add files via upload

---
+++ FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/FptlsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.FPTLSPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptlsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FptlsPlatformApplication.class, args);
+	}
+
+}
---
+++ FPTLSPlatform/src/test/java/com/example/FPTLSPlatform/FptlsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FPTLSPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptlsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
