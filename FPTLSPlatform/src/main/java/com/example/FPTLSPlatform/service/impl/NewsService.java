757d0207ffb7c2b71b89ba5fb7bebc2e834fa22a
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-11-08 22:24:58 +0700

    api news

--- FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/service/impl/NewsService.java
+++ FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/service/impl/NewsService.java
@@ -6,6 +6,8 @@ import com.example.FPTLSPlatform.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,7 +18,10 @@ public class NewsService {
     private NewsRepository newsRepository;
 
     public NewsDTO createNews(NewsDTO newsDTO) {
+        newsDTO.setDate(LocalDate.now());
+        newsDTO.setTime(LocalTime.now());
         News news = mapDTOToEntity(newsDTO);
+
         News savedNews = newsRepository.save(news);
         return mapEntityToDTO(savedNews);
     }
