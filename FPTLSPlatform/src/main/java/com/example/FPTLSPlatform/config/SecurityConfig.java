018af6defd3552d87f4acd7a4651c501f93b1bcd
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-12-08 23:49:30 +0700

    webconfig

--- FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/config/SecurityConfig.java
+++ FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.FPTLSPlatform.config;
 
-
 import com.example.FPTLSPlatform.filter.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -29,48 +31,60 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            // Allow all origins, methods, and headers
+            configuration.setAllowedOrigins(Arrays.asList("*"));
+            configuration.setAllowedMethods(Arrays.asList("*"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+            return configuration;
+        };
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(corsConfigurationSource())  // Enable CORS globally
+                .and()
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login", "/applications/**", "/auth/register-student", "/auth/register-teacher", "/forgotpassword/**", "api/**", "/auth/confirm-otp", "/feedback/comments", "/auth/forgot-password", "/auth/confirm-otpForgot", "/auth/reset-password", "/ws/**").permitAll() // Publicly accessible endpoints
-//                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/categories", "/categories/{id}").permitAll()
-//                        .requestMatchers("/staff/**").hasAuthority("STAFF")
-//                        .requestMatchers("/teacher/**").hasAuthority("TEACHER")
-//                        .requestMatchers("/student/**").hasAuthority("STUDENT")
-//                        .requestMatchers("/applications/staff").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.GET, "/courses").hasAnyAuthority("TEACHER", "STAFF", "STUDENT")
-//                        .requestMatchers(HttpMethod.POST, "/courses").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.PUT, "/courses/{courseCode}").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.DELETE, "/courses/{courseCode}").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.POST, "/categories").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.PUT, "/categories").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.POST, "/categories").hasAuthority("STAFF")
-//                        .requestMatchers("/classes/byCourse/{courseCode}").hasAnyAuthority("STUDENT", "TEACHER")
-//                        .requestMatchers("/classes/StatusCompleted").hasAnyAuthority("STAFF", "ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/classes/getByClassId/{classId}").hasAnyAuthority("STUDENT", "TEACHER")
-//                        .requestMatchers(HttpMethod.GET, "/classes").hasAnyAuthority("STUDENT", "ADMIN")
-//                        .requestMatchers("/classes/teacher/{teacherName}").hasAuthority("STUDENT")
-//                        .requestMatchers("/classes/{classId}/students").hasAnyAuthority("STAFF", "STUDENT", "TEACHER")
-//                        .requestMatchers(HttpMethod.PUT, "/classes/{classId}").hasAuthority("TEACHER")
-//                        .requestMatchers("/classes/my-classes").hasAuthority("TEACHER")
-//                        .requestMatchers(HttpMethod.POST, "/classes").hasAuthority("TEACHER")
-//                        .requestMatchers("classes/confirm-classes/").hasAuthority("TEACHER")
-//                        .requestMatchers("orders/**").hasAnyAuthority("STAFF", "STUDENT", "ADMIN")
-//                        .requestMatchers("feedback/order/{orderId}/submit").hasAuthority("STUDENT")
-//                        .requestMatchers("feedback/class/{classId}/summary").hasAuthority("STAFF")
-//                        .requestMatchers("/api/feedback-question").hasAuthority("STAFF")
-//                        .requestMatchers("/api/feedback-category").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.POST, "/slots").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.PUT, "/slots/{slotId}").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.DELETE, "/slots/{slotId}").hasAuthority("STAFF")
-//                        .requestMatchers(HttpMethod.GET, "/slots").hasAnyAuthority("STAFF", "STUDENT", "TEACHER", "ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/slots/{slotId}").hasAnyAuthority("STAFF", "STUDENT", "TEACHER")
-//                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
-
-
-                        .anyRequest().permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/categories", "/categories/{id}").permitAll()
+                        .requestMatchers("/staff/**").hasAuthority("STAFF")
+                        .requestMatchers("/teacher/**").hasAuthority("TEACHER")
+                        .requestMatchers("/student/**").hasAuthority("STUDENT")
+                        .requestMatchers("/applications/staff").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.GET, "/courses").hasAnyAuthority("TEACHER", "STAFF", "STUDENT")
+                        .requestMatchers(HttpMethod.POST, "/courses").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.PUT, "/courses/{courseCode}").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.DELETE, "/courses/{courseCode}").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.POST, "/categories").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.PUT, "/categories").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.POST, "/categories").hasAuthority("STAFF")
+                        .requestMatchers("/classes/byCourse/{courseCode}").hasAnyAuthority("STUDENT", "TEACHER")
+                        .requestMatchers("/classes/StatusCompleted").hasAnyAuthority("STAFF", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/classes/getByClassId/{classId}").hasAnyAuthority("STUDENT", "TEACHER")
+                        .requestMatchers(HttpMethod.GET, "/classes").hasAnyAuthority("STUDENT", "ADMIN")
+                        .requestMatchers("/classes/teacher/{teacherName}").hasAuthority("STUDENT")
+                        .requestMatchers("/classes/{classId}/students").hasAnyAuthority("STAFF", "STUDENT", "TEACHER")
+                        .requestMatchers(HttpMethod.PUT, "/classes/{classId}").hasAuthority("TEACHER")
+                        .requestMatchers("/classes/my-classes").hasAuthority("TEACHER")
+                        .requestMatchers(HttpMethod.POST, "/classes").hasAuthority("TEACHER")
+                        .requestMatchers("classes/confirm-classes/").hasAuthority("TEACHER")
+                        .requestMatchers("orders/**").hasAnyAuthority("STAFF", "STUDENT", "ADMIN")
+                        .requestMatchers("feedback/order/{orderId}/submit").hasAuthority("STUDENT")
+                        .requestMatchers("feedback/class/{classId}/summary").hasAuthority("STAFF")
+                        .requestMatchers("/api/feedback-question").hasAuthority("STAFF")
+                        .requestMatchers("/api/feedback-category").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.POST, "/slots").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.PUT, "/slots/{slotId}").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.DELETE, "/slots/{slotId}").hasAuthority("STAFF")
+                        .requestMatchers(HttpMethod.GET, "/slots").hasAnyAuthority("STAFF", "STUDENT", "TEACHER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/slots/{slotId}").hasAnyAuthority("STAFF", "STUDENT", "TEACHER")
+                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -91,4 +105,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
