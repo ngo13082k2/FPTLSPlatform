0f0a391203569e6f0ec6a91992c92f2f0f913c7d
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-12-09 01:37:06 +0700

    webconfig

--- FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/config/WebConfig.java
+++ FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/config/WebConfig.java
@@ -1,18 +1,18 @@
-//package com.example.FPTLSPlatform.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://lss-front-end.vercel.app", "http://localhost:5173")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.example.FPTLSPlatform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://lss-front-end.vercel.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
