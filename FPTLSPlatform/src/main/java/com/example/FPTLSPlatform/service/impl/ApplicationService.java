579137049ddb600184f5a283ab789e4aa8d32d2d
Eus <133019198+phudx1012@users.noreply.github.com>
2024-12-09 11:02:26 +0700

    update SecurityConfig

--- FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/service/impl/ApplicationService.java
+++ FPTLSPlatform/src/main/java/com/example/FPTLSPlatform/service/impl/ApplicationService.java
@@ -115,6 +115,7 @@ public class ApplicationService implements IApplicationService {
 
                 certificates.add(certificate);
             }
+            application.setCertificates(certificates);
         }
     }
 
